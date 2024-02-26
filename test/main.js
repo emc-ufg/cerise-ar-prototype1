d3b0e5dd1160b81c27296eda72adbba8b9e9fde1
Matheus Carlos <78445685+Vuttley@users.noreply.github.com>
2024-02-26 19:34:39 -0300

    Update main.js
    Interface improvement
    Screen size based positioning and sizing
    Attention to image load timing

--- test/main.js
+++ test/main.js
@@ -6,53 +6,91 @@ document.addEventListener("DOMContentLoaded", () => {
     const start = async () => {
         const mindarThree = new MindARThree({
             container: document.body,
-            imageTargetSrc: "multiples.mind", //+50 targets
+            imageTargetSrc: "all.mind", //multiples.mind -> test 50+ anchors
         });
 
         const {renderer, scene, camera} = mindarThree;
 
-        let current_anchor = null;
+        var current_anchor = null; //current index for targeted anchor (will be aways 0 after remove other anchors)
 
-        let mode = null;
+        var mode = null; //mode of view that changes with the buttons
 
-        let anchorIndex = null;
+        var anchorIndex = null; //the original index of the targeted anchor
+
+        var lockIndex = false;
+
+        const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        const screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+        //updating space sense with screen parameters
+        var aspectratio = screenWidth/screenHeight;
+        renderer.setSize(screenWidth, screenHeight, false);
+        camera.aspect = aspectratio;
+        camera.updateProjectionMatrix();
+        
+        function getobjectLimits(distance){ //we have a 3d space and the object visualization in screen depends on its placement
+            let screeninUse = null;
+            if (aspectratio>=1){
+                screeninUse = 0.7; //empiric value, test and change as wish
+            }
+            else{
+                screeninUse = 0.8; //empiric value
+            }
+            let vFOV = THREE.MathUtils.degToRad( camera.getEffectiveFOV() );
+            let height = Math.tan( vFOV / 2 ) * distance; //already half of the actual height
+            let width = height * camera.aspect;
+            return { width: screeninUse*width, height: screeninUse*height };
+        };
 
         //text
 
-        let textmsg = [];
+        var textmsg = [];
 
         textmsg.push('anchor0', 'anchor1');
 
-        let txtloaded = null;
+        var txtloaded = null;
 
-        function addtxt(index){
-            const cv = document.createElement( 'canvas' );
-            cv.width = window.innerWidth*0.2;
-            cv.height = window.innerHeight*0.1;
-            const ctx = cv.getContext( '2d' );
-            ctx.fillStyle = '#fefefe'; 
-            ctx.fillRect( 0, 0, cv.width, cv.height );
+        function addtxt(index) {
+            const cv = document.createElement('canvas');
+            const canvasWidth = window.innerWidth * 0.2;
+            const canvasHeight = window.innerHeight * 0.1;
+            cv.width = canvasWidth;
+            cv.height = canvasHeight;
+            const ctx = cv.getContext('2d');
+            ctx.fillStyle = '#fefefe';
+            ctx.fillRect(0, 0, cv.width, cv.height);
             ctx.fillStyle = '#129912';
             ctx.textAlign = 'left';
             ctx.textBaseline = 'middle';
-            ctx.font = 'bold 6vh Arial';
-            let txtstring = textmsg[anchorIndex];
-            ctx.fillText(txtstring, 0, 0.5 * cv.height );
-            const txtGeometry = new THREE.BoxGeometry( 2.4, 0.8, 0.1 );
-            const cvTexture = new THREE.Texture( cv );
+            const txtstring = textmsg[anchorIndex];
+            const maxFontSize = 0.5*Math.min(canvasWidth, canvasHeight);
+            let fontSize = maxFontSize;
+            while (fontSize > 1) { //adjust fontsize till the text is all in canvas element
+                ctx.font = `bold ${fontSize}px Arial`;
+                const textWidth = ctx.measureText(txtstring).width;
+                if (textWidth < canvasWidth) {
+                    break;
+                }
+                fontSize--;
+            }
+            ctx.font = `bold ${fontSize}px Arial`;
+            ctx.fillText(txtstring, 0, 0.5 * cv.height);
+            const txtGeometry = new THREE.BoxGeometry(2.4, 0.8, 0.1);
+            const cvTexture = new THREE.Texture(cv);
             cvTexture.needsUpdate = true;
-            const spineMat = new THREE.MeshPhongMaterial( { color: 0xa5800e } );
-            const cvMaterial = new THREE.MeshBasicMaterial( { map: cvTexture, transparent: true, opacity:0.8  } );
-            const cvMaterials = [ spineMat, spineMat, spineMat, spineMat, cvMaterial, cvMaterial ]; 
-            txtloaded = new THREE.Mesh( txtGeometry, cvMaterials );
+            const spineMat = new THREE.MeshPhongMaterial({ color: 0xa5800e });
+            const cvMaterial = new THREE.MeshBasicMaterial({ map: cvTexture, transparent: true, opacity: 0.8 });
+            const cvMaterials = [spineMat, spineMat, spineMat, spineMat, cvMaterial, cvMaterial];
+            txtloaded = new THREE.Mesh(txtGeometry, cvMaterials);
             txtloaded.rotation.y = 0;
             let anchornow = mindarThree.anchors[index];
             anchornow.group.add(txtloaded);
-        };
+        }
+        
 
         //3d models
 
-        let modelspath = [];
+        var modelspath = [];
 
         modelspath.push('applications/assets/models/musicband-bear/scene.gltf', 'applications/assets/models/musicband-raccoon/scene.gltf');
 
@@ -70,74 +108,112 @@ document.addEventListener("DOMContentLoaded", () => {
 
         //images
 
-        let imagepaths = [];
+        var imagepaths = [];
 
         imagepaths.push('applications/assets/targets/musicband-bear.png', 'applications/assets/targets/musicband-raccoon.png');
 
-        let imageloaded = null;
+        var imageloaded = null;
 
         function addimage(index){
-            const imagegeometry = new THREE.PlaneGeometry(2,2);
             const imagetextureLoader = new THREE.TextureLoader();
-            const imagetexture = imagetextureLoader.load(imagepaths[anchorIndex]);
-            const imagematerial = new THREE.MeshBasicMaterial({ map: imagetexture, transparent: true, opacity: 1 });
-            imageloaded = new THREE.Mesh(imagegeometry, imagematerial);
-            let anchornow = mindarThree.anchors[index];
-            anchornow.group.add(imageloaded);
+            imagetextureLoader.load(imagepaths[anchorIndex], (imagetexture) => {
+                const imagematerial = new THREE.MeshBasicMaterial({ map: imagetexture, transparent: true, opacity: 1 });
+                const imageRatio = imagetexture.image.width / imagetexture.image.height;
+                let imagegeometry;
+                if(aspectratio<1){
+                    imagegeometry = new THREE.PlaneGeometry(imageRatio*screenHeight/400,screenHeight/400);
+                }
+                else{
+                    imagegeometry = new THREE.PlaneGeometry(imageRatio*screenWidth/800,screenWidth/800);
+                }
+                imageloaded = new THREE.Mesh(imagegeometry, imagematerial);
+                let anchornow = mindarThree.anchors[index];
+                anchornow.group.add(imageloaded);
+            });
         };
 
         //adding interaction elements
 
-        const fechargeometry = new THREE.PlaneGeometry(2,2);
-        const fechartextureLoader = new THREE.TextureLoader();
-        const fechartexture = fechartextureLoader.load("fechar.png");
-        const fecharmaterial = new THREE.MeshBasicMaterial({ map: fechartexture, transparent: true, opacity: 1 });
-        const fecharplane = new THREE.Mesh(fechargeometry, fecharmaterial);
+        var interationelementsZ = -12; //-10 already above the camera image
+        var interactionelementsDistance = camera.position.z - interationelementsZ;
+        var interactionelementsLimits = getobjectLimits(interactionelementsDistance);
 
-        const fechar = new THREE.Group();
-        fechar.add(fecharplane);
-        fechar.position.set(0, -3, -12);
-        fechar.rotation.set(0, 0, 0);
-        fechar.userData.clickable = true;
-        fechar.name = "fechar"
+        if(aspectratio>=1){
+            var buttonwidth = 0.2*interactionelementsLimits.width;
+            var buttonheight = 0.2*interactionelementsLimits.height;
+        }
+        else{
+            var buttonwidth = 0.15*interactionelementsLimits.width;
+            var buttonheight = 0.15*interactionelementsLimits.height;
+        }
 
-        const botãogeometry = new THREE.BufferGeometry();
-        const vertices = new Float32Array( [
-            -1.0, -1.0,  0.0, // v0
-             1.0, -1.0,  0.0, // v1
-             0.0,  1.0,  0.0 // v2
-        ] );
-        botãogeometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-        const botãomaterial = new THREE.MeshBasicMaterial({ color: "#0000FF", transparent: true, opacity: 1 });
-        const botãoplane1 = new THREE.Mesh(botãogeometry, botãomaterial);
-        const botãoplane2 = new THREE.Mesh(botãogeometry, botãomaterial);
+        let close;
+        const closetextureLoader = new THREE.TextureLoader();
+        closetextureLoader.load("close2.png", (closetexture) => {
+            let imageratio = closetexture.image.width / closetexture.image.height;
+            const closegeometry = new THREE.PlaneGeometry(buttonheight * imageratio, buttonheight);
+            const closematerial = new THREE.MeshBasicMaterial({ map: closetexture, transparent: true, opacity: 1 });
+            const closeplane = new THREE.Mesh(closegeometry, closematerial);
+            close = new THREE.Group();
+            close.add(closeplane);
+            if (aspectratio>=1){
+                close.position.set(0, (buttonheight - interactionelementsLimits.height), interationelementsZ);
+            }
+            else{
+                close.position.set(0, -interactionelementsLimits.height+buttonheight/2, interationelementsZ);
+            }
+            close.rotation.set(0, 0, 0);
+            close.userData.clickable = true; //necessary for click event
+        });
 
-        const botãodireito = new THREE.Group();
-        botãodireito.add(botãoplane1);
-        botãodireito.position.set(5, -3, -12);
-        botãodireito.rotation.set(0, 0, 3*Math.PI/2);
-        botãodireito.userData.clickable = true;
-        botãodireito.name = "botãodireito"
+        const buttongeometry = new THREE.PlaneGeometry(buttonheight, buttonheight);
 
-        const botãoesquerdo = new THREE.Group();
-        botãoesquerdo.add(botãoplane2);
-        botãoesquerdo.position.set(-5, -3, -12);
-        botãoesquerdo.rotation.set(0, 0, Math.PI/2);
-        botãoesquerdo.userData.clickable = true;
-        botãoesquerdo.name = "botãoesquerdo"
+        let buttonright;
+        const rightbuttontextureLoader = new THREE.TextureLoader();
+        rightbuttontextureLoader.load("arrow.png", (rightbuttontexture) => {
+            const rightbuttonmaterial = new THREE.MeshBasicMaterial({ map: rightbuttontexture, transparent: true, opacity: 1 });
+            const rightbuttonplane = new THREE.Mesh(buttongeometry, rightbuttonmaterial);
+            buttonright = new THREE.Group();
+            buttonright.add(rightbuttonplane);
+            if (aspectratio>=1){
+                buttonright.position.set((interactionelementsLimits.width - buttonwidth), (buttonheight - interactionelementsLimits.height), interationelementsZ);
+            }
+            else{
+                buttonright.position.set((interactionelementsLimits.width - buttonwidth), (-interactionelementsLimits.height+buttonheight/2), interationelementsZ);
+            }
+            buttonright.rotation.set(0, 0, 0);
+            buttonright.userData.clickable = true;
+        });
+
+        let buttonleft;
+        const leftbuttontextureLoader = new THREE.TextureLoader();
+        leftbuttontextureLoader.load("arrowleft.png", (leftbuttontexture) => {
+            const leftbuttonmaterial = new THREE.MeshBasicMaterial({ map: leftbuttontexture, transparent: true, opacity: 1 });
+            const leftbuttonplane = new THREE.Mesh(buttongeometry, leftbuttonmaterial);
+            buttonleft = new THREE.Group();
+            buttonleft.add(leftbuttonplane);
+            if (aspectratio>=1){
+                buttonleft.position.set((-interactionelementsLimits.width + buttonwidth), (buttonheight - interactionelementsLimits.height), interationelementsZ);
+            }
+            else{
+                buttonleft.position.set((-interactionelementsLimits.width + buttonwidth), (-interactionelementsLimits.height+buttonheight/2), interationelementsZ);
+            }
+            buttonleft.rotation.set(0, 0, 0);
+            buttonleft.userData.clickable = true;
+        });   
 
         //interface management
 
-        function abririnterfacenavegacao(){
-            scene.add(botãodireito);
-            scene.add(botãoesquerdo);
-            scene.add(fechar);
+        function opennavigationInterface(){
+            scene.add(buttonright);
+            scene.add(buttonleft);
+            scene.add(close);
         };
 
-        function fecharinterfacenavegacao(){
-            scene.remove(botãodireito);
-            scene.remove(botãoesquerdo);
-            scene.remove(fechar);
+        function closenavigationInterface(){
+            scene.remove(buttonright);
+            scene.remove(buttonleft);
+            scene.remove(close);
         };
 
         //anchor management
@@ -150,12 +226,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         function addallAnchors() {
             return new Promise((resolve) => {
-
                 removeallAnchors();
-        
                 const anchor0 = mindarThree.addAnchor(0);
                 const anchor1 = mindarThree.addAnchor(1);
-
                 mindarThree.anchors.forEach((anchor) => {
                     anchor.onTargetFound = () => {
                         handleTargetFound(anchor);
@@ -164,7 +237,6 @@ document.addEventListener("DOMContentLoaded", () => {
                         handleTargetLost();
                     };
                 });
-        
                 resolve();
             });
         };
@@ -188,28 +260,30 @@ document.addEventListener("DOMContentLoaded", () => {
         };
 
         function handleTargetFound(anchor) {
-            anchorIndex = mindarThree.anchors.indexOf(anchor);
-            addtxt(anchorIndex);
-            mode = 0;
-            abririnterfacenavegacao();
-            remove_other_anchors(anchorIndex);
-            current_anchor = 0;
+            if(!lockIndex){
+                anchorIndex = mindarThree.anchors.indexOf(anchor);
+                addtxt(anchorIndex);
+                remove_other_anchors(anchorIndex);
+                mode = 0;
+                opennavigationInterface();
+                current_anchor = 0;
+                lockIndex = true;
+            }
         };
         
-        function handleTargetLost() {
+        async function handleTargetLost() {
+        };
 
-        }
-
-        async function fecharHandler() {
+        async function closeHandler() {
             if (current_anchor !== null && mindarThree.anchors[current_anchor]) {
                 await removeAllObjectsFromGroup(mindarThree.anchors[current_anchor].group);
             }
-            fecharinterfacenavegacao();
+            closenavigationInterface();
             current_anchor = null;
             await addallAnchors();
         };
 
-        function direitahandler(){
+        function righthandler(){
             if (current_anchor !== null && mindarThree.anchors[current_anchor]) {
                 let index = current_anchor;
                 if (mode === 0){
@@ -233,7 +307,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         };
 
-        function esquerdahandler(){
+        function lefthandler(){
             if (current_anchor !== null && mindarThree.anchors[current_anchor]) {
                 let index = current_anchor;
                 if (mode === 0){
@@ -264,7 +338,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.addEventListener("click", (e)=>{
             e.preventDefault();
             console.log("click");
-            var canvasBounds = renderer.domElement.getBoundingClientRect();
+            let canvasBounds = renderer.domElement.getBoundingClientRect();
             const mouseX = ( ( e.clientX - canvasBounds.left ) / ( canvasBounds.right - canvasBounds.left ) ) * 2 - 1; 
             const mouseY = - ( ( e.clientY - canvasBounds.top ) / ( canvasBounds.bottom - canvasBounds.top) ) * 2 + 1;
             const mouse = new THREE.Vector3(mouseX, mouseY, -12);
@@ -277,30 +351,37 @@ document.addEventListener("DOMContentLoaded", () => {
                     o = o.parent;
                 }
                 if (o.userData.clickable){
-                    if (o === botãoesquerdo){
-                        esquerdahandler();
+                    if (o === buttonleft){
+                        lefthandler();
                     }
-                    if (o === botãodireito){
-                        direitahandler();
+                    if (o === buttonright){
+                        righthandler();
                     }
-                    if (o === fechar){
-                        fecharHandler();
+                    if (o === close){
+                        closeHandler();
+                        lockIndex = false;
                     }
                 }
             }
         });
 
-        // FPS Counter
+        window.addEventListener('resize', () => {
+            const newScreenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            const newScreenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+            renderer.setSize(newScreenWidth, newScreenHeight, false);
+            camera.aspect = newScreenWidth / newScreenHeight;
+            camera.updateProjectionMatrix();
+        });
 
-        let fpsCounter = document.createElement('div');
+        var fpsCounter = document.createElement('div');
         fpsCounter.style.position = 'absolute';
         fpsCounter.style.top = '10px';
         fpsCounter.style.left = '10px';
         fpsCounter.style.color = '#ffffff';
         document.body.appendChild(fpsCounter);
 
-        let frameCount = 0;
-        let lastTime = performance.now();
+        var frameCount = 0;
+        var lastTime = performance.now();
 
         function updateFPSCounter() {
             const currentTime = performance.now();
