40dfcbde9e8123152ff202a627337faeac298742
cgalvaojr1976 <cgalvao@ufg.br>
2024-03-28 23:01:00 -0300

    Delete course folder Add QRCode reader

---
+++ qrcode_reader/qrcodereader.js
@@ -0,0 +1,22 @@
+var resultContainer = document.getElementById('qr-reader-results');
+var lastResult, countResults = 0;
+
+function onScanSuccess(decodedText, decodedResult) {
+    if (decodedText !== lastResult) {
+        ++countResults;
+        lastResult = decodedText;
+        // Handle on success condition with the decoded message.
+        console.log(`Scan result ${decodedText}`, decodedResult);
+        document.getElementById("Result").innerHTML = decodedText;
+
+    }
+}
+
+function onScanError(errorMessage) {
+    // handle on error condition, with error message
+    console.log(errorMessage);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
